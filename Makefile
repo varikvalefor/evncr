7288f7771c33fe2f4f04ae3d11a53aaac055cffc
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2020-11-26 03:22:44 -0500

    EVANNOUNCER is now written in Haskell.

--- Makefile
+++ Makefile
@@ -1,11 +1,16 @@
 FNAME = evncr
-CXX = c++
-CFLAGS = -g -O3 -o $(FNAME)
+HASK = ghc
+FLAGS = -O3
+FNAME = evncr
 
-install: main.cc
-	$(CXX) $(CFLAGS) main.cc
+install: evncr.hs
+	$(HASK) $(FLAGS) evncr.hs
 	mv $(FNAME) /usr/local/bin/
 	chmod 711 /usr/local/bin/$(FNAME)
 	cp evncr.1 /usr/local/man/man1/$(FNAME).1
 	ls /usr/local/share/$(FNAME) || mkdir /usr/local/share/$(FNAME)
 	cp -r sound /usr/local/share/$(FNAME)/
+uninstall:
+	rm /usr/local/bin/$(FNAME)
+	rm -r /usr/local/share/$(FNAME)
+	rm /use/local/man/man1/$(FNAME).1
---
+++ evncr.hs
@@ -0,0 +1,46 @@
+-- TODO: IMPLEMENT SLEEPING.
+
+import System.Process;
+import System.Environment;
+import Text.Printf;
+
+thingToInt :: Char -> [Int];
+thingToInt theShift = [(prefixOne theShift), (prefixTwo theShift), (meatAndPotato theShift)];
+
+prefixOne :: Char -> Int;
+prefixOne theShift
+             | ((fromEnum theShift)) < 58 && ((fromEnum theShift) >= 48) = 129
+             | otherwise = 128
+
+prefixTwo :: Char -> Int;
+prefixTwo theShift
+             | shifter < 48 = 0
+             | shifter < 58 = 1
+             | shifter < 65 = 0
+             | shifter < 91 = 3
+             | shifter < 97 = 0
+             | shifter < 123 = 4
+             where shifter = (fromEnum theShift)
+
+meatAndPotato :: Char -> Int;
+meatAndPotato theShift
+                 | (shifter > 96) && (shifter < 123) = (shifter - 32)
+                 | shifter `elem` [62,93,125] = (shifter - 2)
+                 | shifter == 41 = 40
+                 | otherwise = shifter
+                 where shifter = (fromEnum theShift)
+
+toFileName :: Int -> [Char];
+toFileName theShift = ("./" ++ (show theShift) ++ ".wav");
+
+outputSound :: [Int] -> [IO String];
+outputSound intRep = map (playFile) (map toFileName intRep);
+
+playFile :: [Char] -> IO String;
+playFile filename = readProcess "mplayer " [filename] "";
+
+main = do
+  theInput <- getLine;
+  print (map (thingToInt) theInput);
+  let fuck = map (outputSound) (map (thingToInt) theInput);
+  printf "";

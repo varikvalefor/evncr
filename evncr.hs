50fa9dd7176f8608b818337d61c5f770d6b8b012
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2020-12-04 23:37:09 -0500

    Some indentation is changed to improve readability.

--- evncr.hs
+++ evncr.hs
@@ -13,31 +13,31 @@ thingToInt theShift = [(prefixOne theShift), (prefixTwo theShift), (meatAndPotat
 --"prefix" of X, e.g., "LATIN" or "ARABIC".
 prefixOne :: Char -> Int;
 prefixOne theShift
-             | (shifter < 58) && (shifter >= 48) = 129
-             | otherwise = 128
-             where shifter = (fromEnum theShift)
+  | (shifter < 58) && (shifter >= 48) = 129
+  | otherwise = 128
+  where shifter = (fromEnum theShift)
 
 --For all characters X, prefixTwo X is equal to the second
 --"prefix" of X, e.g., "MAJUSCULE" or "MINUSCULE".
 prefixTwo :: Char -> Int;
 prefixTwo theShift
-             | shifter < 48 = 0
-             | shifter < 58 = 1
-             | shifter < 65 = 0
-             | shifter < 91 = 3
-             | shifter < 97 = 0
-             | shifter < 123 = 4
-             where shifter = (fromEnum theShift)
+  | shifter < 48 = 0
+  | shifter < 58 = 1
+  | shifter < 65 = 0
+  | shifter < 91 = 3
+  | shifter < 97 = 0
+  | shifter < 123 = 4
+  where shifter = (fromEnum theShift)
 
 -- For all characters X, meatAndPotato converts X into the
 -- "simplified" ASCII representation of X.
 meatAndPotato :: Char -> Int;
 meatAndPotato theShift
-                 | (shifter > 96) && (shifter < 123) = (shifter - 32)
-                 | shifter `elem` [62,93,125] = (shifter - 2)
-                 | shifter == 41 = 40
-                 | otherwise = shifter
-                 where shifter = (fromEnum theShift)
+  | (shifter > 96) && (shifter < 123) = (shifter - 32)
+  | shifter `elem` [62,93,125] = (shifter - 2)
+  | shifter == 41 = 40
+  | otherwise = shifter
+  where shifter = (fromEnum theShift)
 
 toFileName :: Int -> [Char];
 toFileName theShift = ("/usr/local/share/evncr/sound/" ++ (show theShift) ++ ".wav");

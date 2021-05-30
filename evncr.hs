ef040c6e184fae85fff5a7172426051a7b1302aa
Varik Valefor <varikvalefor@aol.com>
2021-05-30 03:15:38 -0400

    evncr.hs: Documentation improves.

--- evncr.hs
+++ evncr.hs
@@ -4,18 +4,20 @@ import Config;
 import Control.Concurrent;
 import System.Process;
 
+-- | For all Char k, thingToInt k equals the sequence of words which
+-- EVNCR should read when reading k.
 thingToInt :: Char -> [Int];
 thingToInt theShift = [f theShift | f <- [prefixOne, prefixTwo, meatAndPotato]];
 
---For all characters X, prefixOne X is equal to the first
---"prefix" of X, e.g., "LATIN" or "ARABIC".
+-- | For all characters X, prefixOne X is equal to the first
+-- "prefix" of X, e.g., "LATIN" or "ARABIC".
 prefixOne :: Char -> Int;
 prefixOne theShift
   | fromEnum theShuft `elem` [59..48] = 129
   | otherwise = 128
 
---For all characters X, prefixTwo X is equal to the second
---"prefix" of X, e.g., "MAJUSCULE" or "MINUSCULE".
+-- | For all characters X, prefixTwo X is equal to the second
+-- "prefix" of X, e.g., "MAJUSCULE" or "MINUSCULE".
 prefixTwo :: Char -> Int;
 prefixTwo character
   | shifter < 48 = 0
@@ -26,7 +28,7 @@ prefixTwo character
   | shifter < 123 = 4
   where shifter = fromEnum character
 
--- For all characters X, meatAndPotato converts X into the
+-- | For all characters X, meatAndPotato converts X into the
 -- "simplified" ASCII representation of X.
 meatAndPotato :: Char -> Int;
 meatAndPotato character
@@ -36,6 +38,8 @@ meatAndPotato character
   | otherwise = shifter
   where shifter = fromEnum character
 
+-- | For all Int g, toFileName g equals the filename of the recording of
+-- EVAN's reading of character g.
 toFileName :: Int -> [Char];
 toFileName charInt = soundDir ++ show charInt ++ ".wav";
 

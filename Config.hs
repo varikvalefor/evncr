942671e75b6a3cc422a0c2622cab5cae5c9c6fc4
Varik Valefor <varikvalefor@aol.com>
2021-06-03 17:04:53 -0400

    Config.hs: Spacing improves.

--- Config.hs
+++ Config.hs
@@ -1,16 +1,19 @@
 module Config where
-  -- | delay_intraChar denotes the delay between readings of
-  -- intra-character phrases, e.g., "ARABIC" and "MAJUSCULE".
-  -- Times are given in microseconds.
-  delay_intraChar :: Int;
-  delay_intraChar = 125 * 10^3;
-  -- | delay_intraChar denotes the delay between readings of
-  -- characters, e.g., "LATIN MAJUSCULE ALFA" and "LATIN MAJUSCULE
-  -- WHISKEY".
-  -- Times are given in microseconds.
-  delay_interChar :: Int;
-  delay_interChar = 250 * 10^3;
-  -- | soundDir is the directory which contains the voice samples of
-  -- EVANNOUNCER.
-  soundDir :: FilePath;
-  soundDir = "/usr/local/share/evncr/";
+
+-- | delay_intraChar denotes the delay between readings of
+-- intra-character phrases, e.g., "ARABIC" and "MAJUSCULE".
+-- Times are given in microseconds.
+delay_intraChar :: Int;
+delay_intraChar = 125 * 10^3;
+
+-- | delay_intraChar denotes the delay between readings of
+-- characters, e.g., "LATIN MAJUSCULE ALFA" and "LATIN MAJUSCULE
+-- WHISKEY".
+-- Times are given in microseconds.
+delay_interChar :: Int;
+delay_interChar = 250 * 10^3;
+
+-- | soundDir is the directory which contains the voice samples of
+-- EVANNOUNCER.
+soundDir :: FilePath;
+soundDir = "/usr/local/share/evncr/";
